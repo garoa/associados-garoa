80ac75c4d4ddd26a99b5b7118bcb1ffe4c06c930
Hugo Borges - agaelebe <agaelebe@gmail.com>
2013-06-28 16:53:42 -0300

    first commit to Associados-garoa app, adding all rails project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssociadosGaroa::Application.config.secret_key_base = 'ed3424c2658e5489031a85f75bde520524b35c5e65c69ef774456386fd33b315e42e16f3090b9e7b7c210af23f9c3b6b2fd0fd3678fc85db16e23167991d6916'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssociadosGaroa::Application.config.session_store :cookie_store, key: '_associados-garoa_session'
