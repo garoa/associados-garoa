7f010661b920030b270a0f11a76d300c973c8b77
Hugo Borges - agaelebe <agaelebe@gmail.com>
2015-02-02 14:39:32 -0200

    Updating mail to add information about the yearly payment in february

--- app/mailers/monthly_fee_mailer.rb
+++ app/mailers/monthly_fee_mailer.rb
@@ -1,9 +1,16 @@
 class MonthlyFeeMailer < ActionMailer::Base
-  default from: 'tesoureiro@garoa.net.br', reply_to: 'tesoureiro@garoa.net.br'
+  TREASURER_EMAIL = 'tesoureiro@garoa.net.br'
+
+  default from:        TREASURER_EMAIL,
+          reply_to:    TREASURER_EMAIL,
+          return_path: TREASURER_EMAIL,
+          sender:      TREASURER_EMAIL
 
   def remember_to_pay(user)
       @aka = user.aka
       @current_year_and_month = I18n.l(Date.today, :format => :year_month)
+      @february = (Date.today.month == 2)
+
       mail(to: user.email,
            subject: "Garoa Hacker Clube - Mensalidade do mês de #{@current_year_and_month}")
     end
