dd6442c72e2929ef92fb8d96ce5e2e71e98870b0
Hugo Borges - agaelebe <agaelebe@gmail.com>
2015-12-03 10:41:51 -0200

    send monthly membership email at 7pm

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,7 +23,7 @@ env :PATH, ENV['PATH']
 
 set :output, { error: 'tmp/error.log', standard: 'tmp/cron.log' }
 
-every :month, at: 'start of the month at 12:00am' do
+every :month, at: 'start of the month at 7:00pm' do
   rake "email:monthly_membership"
 end
 
